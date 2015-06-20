8db82c5f6d92bb73ed4c6c8b521cc538146372d7
Wojciech Piątkowski <chmurson@gmail.com>
2015-06-20 15:32:39 +0200

    receive mails and get their attachments

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var bb = require('express-busboy');
 var app = express();
+bb.extend(app,{
+  upload: true,
+  path: __dirname + "/file-uploads"
+});
 
 var braintree = require('./braintree');
 
@@ -8,6 +13,14 @@ app.get('/', function (req, res) {
   braintree.getClientToken().done(function(token) {
     res.send('Hello World! Token ' + token);
   });
+});
+
+//E-MAIL RETRIEVING - registering a charity
+app.post('/inbound-mail',function(req, res){
+  console.log('Mail retrieved');
+  console.log(req);
+
+  res.send(200);
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
