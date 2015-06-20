f96338c325168f7c3039d97ca5dc99f7bdd918c2
Tomusdrw <tomusdrw@gmail.com>
2015-06-20 16:47:40 +0200

    Adding some consol.e.logs.

--- app.js
+++ app.js
@@ -42,8 +42,9 @@ app.get('/api/client-token', function (req, res) {
 
 app.post('/payment-methods', function (req, res) {
   var nonce = req.body.payment_method_nonce;
-
+  console.log('Creating payment: ' + nonce);
   braintree.createPayment(300, nonce).done(function (paymentResult) {
+    console.log(paymentResult);
     res.send(paymentResult);
   });
 });
