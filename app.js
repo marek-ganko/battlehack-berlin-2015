f17a98fc8134ee196ee14333048566997df52036
Tomusdrw <tomusdrw@gmail.com>
2015-06-20 16:43:37 +0200

    Fixing domain.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ bb.extend(app, {
 
 // CORS middleware
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
