fc6347f13b02cd19d769f7370d15aacbd4e35353
Tomusdrw <tomusdrw@gmail.com>
2015-06-21 06:41:01 +0200

    Removing random positions.

--- app/charities.js
+++ app/charities.js
@@ -6,12 +6,6 @@ var braintree = require('./services/braintree');
 
 router.get('/', function (req, res) {
   charities.getCharities().done(function (data) {
-    data.forEach(function (item) {
-      item.coordinates = {
-        latitude: 52.51666666666667 + (Math.random() - 0.5),
-        longitude: 13.4 + (Math.random() - 0.5)
-      };
-    });
     res.send(data);
   });
 });
