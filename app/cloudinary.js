1b31b1c596f8da1ed8b052c38e2c315069ad96e1
Wojciech Piątkowski <chmurson@gmail.com>
2015-06-21 07:25:50 +0200

    adding attachemnt url to image

--- app/cloudinary.js
+++ app/cloudinary.js
@@ -10,17 +10,24 @@ cloudinary.config({
 module.exports = {
 
   /**
-   * @param stream
    * @return {{stream:FileStream,promise: Promise}}
    */
-  uploadStream: function (stream) {
+  uploadStream: function () {
     var deferred = Q.defer();
+
+    var uploaderStream = cloudinary.uploader.upload_stream(function (result) {
+      if (result.error){
+        deferred.reject()
+      }else{
+        deferred.resolve(result);
+      }
+    },function(error){
+      deferred.reject(error);
+    });
 
     return {
       promise: deferred.promise,
-      stream: cloudinary.uploader.upload_stream(function (result) {
-        deferred.resolve(result);
-      })
+      stream: uploaderStream
     };
   }
 };
